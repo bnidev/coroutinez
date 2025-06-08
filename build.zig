9590a189009dfe3641ab56d575a5fdc2f67890f3
Florian Petautschnig <florian@MacBook-Air-von-Florian.local>
2025-06-08 15:35:20 +0200

    update lib name in build.zig

--- build.zig
+++ build.zig
@@ -33,7 +33,7 @@ pub fn build(b: *std.Build) void {
     // for actually invoking the compiler.
     const lib = b.addLibrary(.{
         .linkage = .static,
-        .name = "zigtest",
+        .name = "azync",
         .root_module = lib_mod,
     });
 
